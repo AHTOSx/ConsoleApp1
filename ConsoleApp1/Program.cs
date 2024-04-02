11169cf5ac3b0fd6c349f24f66b116192dff53d7
b22300658 <b22300658@IUTA-WS-TP02-15>
2024-04-02 13:55:45 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez la longueur du rectangle :");
+            double longueur = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Entrez la largeur du rectangle :");
+            double largeur = Convert.ToDouble(Console.ReadLine());
+
+            double aire = CalculerAire(longueur, largeur);
+            Console.WriteLine("L'aire vaut " + aire);
+
+        }
+
+        static double CalculerAire(double longueur, double largeur)
+        {
+            return longueur * largeur;
+        }
+
+        static double CalculerLongueur(double aire, double largeur)
+        {
+            return aire / largeur;
+        }
+    }
+}
